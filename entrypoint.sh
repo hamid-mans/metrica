324df481aa6685210b0baf063179b10aa39b5524
hamid <mansouri.hamid23@gmail.com>
2026-05-08 22:48:55 +0200

    Editor

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,25 +1,19 @@
 #!/bin/sh
-set -e
 
 cd /valori
 
 echo "> En attente de la BDD..."
 until mysqladmin ping -h db -u root --password=roothamid123 --silent; do
+    echo "BDD pas encore prête..."
     sleep 2
 done
 echo "✓ BDD OK !"
 
-# Update Database
-echo "> Mise à jour BDD..."
+# On lance les commandes une par une, sans 'set -e' pour que le script continue quoi qu'il arrive
+echo "> Commandes Symfony..."
 php bin/console doctrine:schema:update --force --no-interaction
-
-# Assets
-echo "> Préparation des assets..."
 php bin/console assets:install public --no-interaction
+php bin/console cache:clear
 
-# --- ON COMMENTE LA LIGNE SUIVANTE ---
-# php bin/console asset-map:compile
-# -------------------------------------
-
-echo "✓ Symfony prêt ! Démarrage du serveur..."
+echo "✓ Tentative de démarrage final..."
 exec "$@"
