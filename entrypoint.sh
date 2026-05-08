06787e16c38e19ce0d5506922a2cbe87a9af617b
hamid <mansouri.hamid23@gmail.com>
2026-05-08 22:44:11 +0200

    Editor

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,25 +1,30 @@
 #!/bin/sh
 set -e
+
 cd /valori
+
 echo "> En attente de la BDD..."
 until mysqladmin ping -h db -u root --password=roothamid123 --silent; do
-    echo "Erreur connexion BDD (entrypoint)..."
     sleep 2
 done
 echo "✓ BDD OK !"
 
-# Installer les dépendances si besoin
+# 1. Installer les dépendances PHP si besoin
 if [ ! -d vendor ]; then
-    composer install --no-dev --optimize-autoloader
+    composer install --no-interaction --optimize-autoloader
 fi
 
+# 2. INSTALLER LES ASSETS (La correction est ici)
+echo "> Installation des assets importmap..."
+php bin/console importmap:install
+
 echo "> Creation schema de BDD..."
-php bin/console doctrine:schema:update --force --complete
+php bin/console doctrine:schema:update --force
 
 echo "> Build assets..."
 php bin/console assets:install public --no-interaction
 php bin/console asset-map:compile
-php bin/console cache:clear --env=prod
+php bin/console cache:clear
 
 echo "✓ Application prete !"
 exec "$@"
